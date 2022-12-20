63d28836ecbc182e55c8ba63adcfbd8954935bfb
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-20 07:34:50 +0000

    LOADING

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,30 @@ import './App.css';
 
 function App() {
 const [todos, setTodos] = useState([])
+const [error, setError] = useState({})
+
+interface Todo {
+  title: string;
+  id:number;
+  completed:boolean;
+}
 
 useEffect(() => {
 fetch('https://jsonplaceholder.typicode.com/todos')
 .then(response => response.json())
-.then(res => console.log(res))
+// the first one 0 until index 9
+.then(res => setTodos(res.slice(0,10)))
+// error handling
+.catch(err => setError(err))
 }, [])
 
 
 
   return (
     <div className="App">
-
+      <>
+      {todos.length > 0 ?  todos.map((todo:Todo )=> console.log(todo.title)): ('Loading....')}
+      </>
     </div>
   );
 }
