44f7d0c943c2bfab9e8fff86df63633caf13ec4f
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-20 07:20:38 +0000

    basic data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
+
 import './App.css';
 
 function App() {
+const [todos, setTodos] = useState([])
+
+useEffect(() => {
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(response => response.json())
+.then(res => console.log(res))
+}, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
